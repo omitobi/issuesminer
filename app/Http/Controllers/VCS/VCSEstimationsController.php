aa70935bc2512342159cc8366d03654361504749
omitobi <omisakin.oluwatobi@gmail.com>
2017-04-26 00:53:06 +0300

    Add wheresIn function to search multiple fields from multiple values

--- app/Http/Controllers/VCS/VCSEstimationsController.php
+++ app/Http/Controllers/VCS/VCSEstimationsController.php
@@ -43,7 +43,7 @@ class VCSEstimationsController extends Utility
             $_revisions_by_date = $revisions->groupBy('Date')->all();
             foreach ($_revisions_by_date as $date =>  $revision)
             {
-                $_revisions_by_imp = $revision->where('vcsFileType.IsOO', '=', 1)->unique('CommitId')->count();
+//                $_revisions_by_imp = $revision->where('vcsFileType.IsOO', '=', 1)->unique('CommitId')->count();
 //                $this->estimations = $_revisions_by_imp;
 //                return false;
 //                $imp_f_count = $imp_f_count + ($revision->vcsFileType->IsImperative) ? 1 : 0;
@@ -55,7 +55,6 @@ class VCSEstimationsController extends Utility
                 $this->populateEstimations($date, 'Avg_Previous_OO_Commits', $revision->where('vcsFileType.IsOO', 1)->unique('CommitId')->count());
                 $this->populateEstimations($date, 'Avg_Previous_XML_Commits', $revision->where('vcsFileType.IsXML', 1)->unique('CommitId')->count());
                 $this->populateEstimations($date, 'Avg_Previous_XSL_Commits', 0);
-
 
                 $this->populateEstimations($date, 'Committer_Previous_Commits', 0);
                 $this->populateEstimations($date, 'Committer_Previous_Imp_Commits', 0);
@@ -80,59 +79,48 @@ class VCSEstimationsController extends Utility
 
 
             }
-            $estimations = $this->estimations;
-            $dates  = array_keys($this->estimations);
-            /*$only  = $this->array_extract($this->estimations, 'Date', 1, 2);
-            */
-            $others  = $this->array_extract($this->estimations, [
-                'Date',
-                'ProjectId',
-                'Avg_Previous_Imp_Commits',
-                'Avg_Previous_OO_Commits',
-                'Avg_Previous_XML_Commits',
-                'Avg_Previous_XSL_Commits',
-                'Committer_Previous_Commits',
-                'Committer_Previous_Imp_Commits',
-                'Committer_Previous_OO_Commits',
-                'Committer_Previous_XML_Commits',
-                'Committer_Previous_XSL_Commits',
-                'Developers_On_Project_To_Date',
-                'Imp_Developers_On_Project_To_Date',
-                'Imperative_Files',
-                'OO_Developers_On_Project_To_Date',
-                'OO_Files',
-                'Total_Developers',
-                'Total_Imp_Developers',
-                'Total_OO_Developers',
-                'Total_XML_Developers',
-                'Total_XSL_Developers',
-                'XML_Developers_On_Project_To_Date',
-                'XML_Files',
-                'XSL_Developers_On_Project_To_Date',
-                'XSL_Files'
-            ]);
-            $this->results = $this->insertOrUpdate($others, 'VCSEstimations');
 
-//            Model::unguard();
-//            $available_for_new = [];
-//            $available_for_new = VCSEstimation::whereIn('Date', $dates)->pluck('Date')->toArray();
-//            $this->results = $available_for_new;
-////            each( function ($model) use ($estimations, &$available_for_new){
-////                $available_for_new[] = $model->Date;
-//////                $es_mod = new VCSEstimation();
-//////                $es_mod->fill($estimations[$model->Date]);
-//////                if(!$es_mod->exists)
-////                $model->update($estimations[$model->Date]);
-////            });
-//
-//           if(!count($available_for_new) || count($available_for_new) !== count($estimations)){
-//               $others = array_except($this->estimations, $available_for_new);
-//               VCSEstimation::insert(
-//                   $others
-//               );
-//           }
-//            Model::reguard();
-            return false;
+            foreach ($revisions as $_revision)
+            {
+                $_revisions_by_imp[] = $_revision->vcsFileType->IsImperative;
+
+            }
+
+            $estimations = $this->estimations;
+
+//            $dates  = array_keys($this->estimations);
+
+//            $others  = $this->array_extract($this->estimations, [
+//                'Date',
+//                'ProjectId',
+//                'Avg_Previous_Imp_Commits',
+//                'Avg_Previous_OO_Commits',
+//                'Avg_Previous_XML_Commits',
+//                'Avg_Previous_XSL_Commits',
+//                'Committer_Previous_Commits',
+//                'Committer_Previous_Imp_Commits',
+//                'Committer_Previous_OO_Commits',
+//                'Committer_Previous_XML_Commits',
+//                'Committer_Previous_XSL_Commits',
+//                'Developers_On_Project_To_Date',
+//                'Imp_Developers_On_Project_To_Date',
+//                'Imperative_Files',
+//                'OO_Developers_On_Project_To_Date',
+//                'OO_Files',
+//                'Total_Developers',
+//                'Total_Imp_Developers',
+//                'Total_OO_Developers',
+//                'Total_XML_Developers',
+//                'Total_XSL_Developers',
+//                'XML_Developers_On_Project_To_Date',
+//                'XML_Files',
+//                'XSL_Developers_On_Project_To_Date',
+//                'XSL_Files'
+//            ]);
+////            $this->results = $this->updatesOrInserts($others, new VCSEstimation());
+//            $this->insertOrUpdate($others, 'VCSEstimations');
+
+//            return false;
         });
 //        $estimations_values = array_values($this->estimations);
 //        list($keys, $estimations_values) = array_divide($this->estimations);
@@ -227,5 +215,52 @@ class VCSEstimationsController extends Utility
 
         return \DB::statement( $sql );
     }
+
+    function updatesOrInserts($attributes, $model)
+    {
+        $available_for_new = [];
+
+        $colection = collect($attributes);
+
+        $first_attr = array_first($attributes);
+
+        $first_keys = array_keys($first_attr);
+
+        $attr = $colection->keyBy($first_keys[0])->all();
+
+        $unique_attribute = $this->array_extract($attr, $first_keys[0]);
+
+        $unique_attribute_values = array_flatten($unique_attribute);
+
+//        $available_for_new = $model->whereIn($first_keys[0], $unique_attribute_values)->pluck($first_keys[0])->toArray();
+//        $results = $available_for_new;
+
+//        $model->whereIn( $first_keys, $attributes)
+//            ->each( function ($_model) use ($attr, $first_keys,  &$available_for_new) {
+//
+//            $available_for_new[] = $_model->{$first_keys[0]} ;
+//
+//            $_model->update( $attr[ $_model->{$first_keys[0]} ] );
+//
+//            });  //this may need chunking so to speed things up
+
+        $model->wheresIn($first_keys, $attributes)
+            ->each( function ($_model) use ($attr, $first_keys,  &$available_for_new) {
+//
+            $available_for_new[] = $_model->{$first_keys[0]} ;
+//
+            $_model->update( $attr[ $_model->{$first_keys[0]} ] );
+//
+            });  //this may need chunking so to speed things up
+
+        if(!count($available_for_new) || count($available_for_new) !== count($attr)){
+            $others = array_except($attr, $available_for_new);
+            $model->insert(
+                $others
+            );
+        }
+    }
+
+
 
 }
--- app/VCSModels/VCSEstimation.php
+++ app/VCSModels/VCSEstimation.php
@@ -16,5 +16,22 @@ class VCSEstimation extends Model
     protected $primaryKey = 'Id';
     protected $table = "VCSEstimations";
 
+    protected $guarded = [];
 //    protected $fillable = ['Id', 'Name'];
+
+    public function wheresIn($keys, $attributes, $model = null, $result = null){
+
+        foreach ($keys as  $key)
+        {
+            $all_values = array_pluck($attributes, $key);
+
+            if(!$model)
+                $model = $this->whereIn($key, $all_values);
+            else
+                $model->whereIn($key, $all_values);
+        }
+
+        return $model;
+
+    }
 }
