cf9b77676a12242ff8f9c398d7996178e6fcd5d1
omitobi <omisakin.oluwatobi@gmail.com>
2017-04-29 18:11:30 +0300

    Add datetouched to vcsfilerevision table

---
+++ database/migrations/2017_04_29_140632_add_datetouched_to_VCSFileRevision.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDatetouchedToVCSFileRevision extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('VCSFileRevision', function (Blueprint $table) {
+            $table->bigInteger('datetouched')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('VCSFileRevision', function (Blueprint $table) {
+            $table->removeColumn('datetouched');
+        });
+    }
+}
